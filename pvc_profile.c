50c9bd23563df72dafab1c854887c69d71d30b40
Ben Olson <matthew.olson@intel.com>
2023-08-01 11:04:48 -0500

    Organizing

--- pvc_profile.c
+++ pvc_profile.c
@@ -12,14 +12,10 @@
 #include <getopt.h>
 #include <poll.h>
 #include <time.h>
-#include <string.h>
-#include <inttypes.h>
 #include <sys/wait.h>
 #include <pthread.h>
 
 #include "drm_helper.h"
-#include "i915_helper.h"
-#include "eustall_helper.h"
 
 /*******************
 * COMMANDLINE ARGS *
@@ -51,7 +47,8 @@ int read_opts(int argc, char **argv) {
 *******************/
 
 int perf_fd;
-pthread_t workload_thread_id;
+uint8_t *perf_buf;
+pthread_t collect_thread_id;
 static int interval_num = 0;
 static int interval_length = 1;
 static int interval_signal;
@@ -59,7 +56,7 @@ timer_t interval_timer;
 static char stopping = 0;
 
 /* This is what happens on a single profiling interval */
-void workload_thread_interval(int s) {
+void collect_thread_interval(int s) {
 }
 
 int init_interval_signal() {
@@ -74,7 +71,7 @@ int init_interval_signal() {
   /* Set up a signal handler for the master.
      The call to sigaddset here blocks the stop signal until an interval is completed. */
   sa.sa_flags = 0;
-  sa.sa_handler = workload_thread_interval;
+  sa.sa_handler = collect_thread_interval;
   sigemptyset(&sa.sa_mask);
   if(sigaction(interval_signal, &sa, NULL) == -1) {
     fprintf(stderr, "Error creating interval signal handler. Aborting.\n");
@@ -119,43 +116,44 @@ int init_interval_signal() {
   return 0;
 }
 
-void workload_thread_stop(int s) {
+void collect_thread_stop(int s) {
   timer_delete(interval_timer);
   stopping = 1;
 }
 
-void *workload_thread_main(void *a) {
+void *collect_thread_main(void *a) {
   int ch, sig;
   sigset_t mask;
+
+  init_interval_signal();
   
-  /* Run a GPGPU FILL workload */
-  
+  perf_buf = malloc(p_user);
 
   /* Wait on SIGTERM */
-/*   sigemptyset(&mask); */
-/*   sigaddset(&mask, SIGTERM); */
-/*   if(sigprocmask(SIG_BLOCK, &mask, NULL) == -1) { */
-/*     fprintf(stderr, "Error blocking SIGTERM. Aborting.\n"); */
-/*     exit(1); */
-/*   } */
-/*   while(sigwait(&mask, &sig) == 0) { */
-/*     if(sig == SIGTERM) { */
-/*       break; */
-/*     } */
-/*   } */
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGTERM);
+  if(sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
+    fprintf(stderr, "Error blocking SIGTERM. Aborting.\n");
+    exit(1);
+  }
+  while(sigwait(&mask, &sig) == 0) {
+    if(sig == SIGTERM) {
+      break;
+    }
+  }
   
   /* Make sure we get at least one interval; if
-      no intervals have run yet, send the interval signal
+      not intervals have run yet, send the interval signal
       manually */
-/*   if(interval_num == 0) { */
-/*     pthread_kill(workload_thread_id, interval_signal); */
-/*   } */
+  if(interval_num == 0) {
+    pthread_kill(collect_thread_id, interval_signal);
+  }
   
-/*   workload_thread_stop(SIGTERM); */
+  collect_thread_stop(SIGTERM);
   return NULL;
 }
 
-int start_workload_thread() {
+int start_collect_thread() {
   int retval;
   sigset_t mask;
 
@@ -168,7 +166,7 @@ int start_workload_thread() {
     return -1;
   }
 
-  retval = pthread_create(&workload_thread_id, NULL, &workload_thread_main, NULL);
+  retval = pthread_create(&collect_thread_id, NULL, &collect_thread_main, NULL);
   if(retval != 0) {
     fprintf(stderr, "Failed to call pthread_create. Something is very wrong. Aborting.\n");
     return -1;
@@ -184,7 +182,6 @@ int start_workload_thread() {
 int main(int argc, char **argv) {
   device_info *devinfo;
   int ret;
-  uint8_t *perf_buf;
   
   /* Grab the i915 driver file descriptor */
   devinfo = open_first_driver();
@@ -194,29 +191,22 @@ int main(int argc, char **argv) {
   }
   
   /* Get information about the device */
+  get_drm_device_id(devinfo);
   if(get_drm_device_info(devinfo) != 0) {
     fprintf(stderr, "Failed to get device info. Aborting.\n");
     exit(1);
   }
   
-  if(i915_query_engines(devinfo->fd, &(devinfo->engine_info)) != 0) {
-    fprintf(stderr, "Failed to get engine info. Aborting.\n");
-    exit(1);
-  }
-  
-  printf("Device ID: 0x%X\n", devinfo->id);
-  
   /* Configure the i915 performance counter file descriptor */
   perf_fd = configure_eustall(devinfo);
   
   /* Begin collecting results */
-  if(start_workload_thread() != 0) {
+  if(start_collect_thread() != 0) {
     fprintf(stderr, "Failed to start the collection thread. Aborting.\n");
     exit(1);
   }
   
   /* Poll for updates to the buffer */
-  perf_buf = malloc(p_user);
   while(stopping == 0) {
     struct pollfd pollfd = {
       .fd = perf_fd,
@@ -224,6 +214,7 @@ int main(int argc, char **argv) {
     };
     ret = poll(&pollfd, 1, 1000);
     if(ret == 0) {
+      printf("Nothing to read!\n");
       continue;
     } else if(ret < 0) {
       fprintf(stderr, "An error occurred while reading the file descriptor. Aborting.\n");
@@ -233,13 +224,12 @@ int main(int argc, char **argv) {
     /* There's things to read */
     ret = read(perf_fd, perf_buf, p_user);
     if(ret > 0) {
-      handle_eustall_samples(perf_buf, ret);
+      printf("I read %d bytes something successfully!\n", ret);
     }
   }
-  printf("Finished\n");
   
-  pthread_kill(workload_thread_id, SIGTERM);
-  pthread_join(workload_thread_id, NULL);
+  pthread_kill(collect_thread_id, SIGTERM);
+  pthread_join(collect_thread_id, NULL);
   
   free_driver(devinfo);
 }
