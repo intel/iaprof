dee3471f72c783a8da5ef46fe923bf1bf057f2e7
Ben Olson <matthew.olson@intel.com>
2023-08-14 15:54:54 -0500

    Tracing now works on programs that mmap their GEMs to write to them

--- experiments/kernel_from_tracing/run_bpftrace.sh
+++ experiments/kernel_from_tracing/run_bpftrace.sh
@@ -12,5 +12,14 @@ cd ${BASE_DIR}/../dummy_workload
 ./bude
 cd ${BASE_DIR}
 
+# IGT_DIR="${BASE_DIR}/../../reference/drivers.gpu.i915.igt-gpu-tools/build/benchmarks/"
+# cd ${IGT_DIR}
+# sudo ./gem_blt &
+# WORKLOAD_PID=$!
+# cd ${BASE_DIR}
+
+sleep 3
+sudo cat /proc/${WORKLOAD_PID}/maps &> maps.txt
+
 kill -TERM ${BPFTRACE_PID}
 wait ${BPFTRACE_PID}
