81ed9b970a8dbd3b9031d5713b361c02945afe1a
Gabriel Munoz <gabriel.munoz@intel.com>
2025-04-29 01:33:54 +0000

    Fix licensing for bpf code to be BSD/GPL
    The dual license is to be applied to the src/collectors/bpf/bpf
    directory.

--- src/collectors/bpf/bpf/main.bpf.c
+++ src/collectors/bpf/bpf/main.bpf.c
@@ -1,19 +1,5 @@
-/*
-Copyright 2025 Intel Corporation
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
+// Copyright 2025 Intel Corporation
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
 
 #if KERNEL_LAUNCH_COLLECTOR == COLLECTOR_driver && GPU_DRIVER == GPU_DRIVER_i915
 #include "i915.h"
@@ -179,4 +165,4 @@ struct {
 #endif
 #endif
 
-char LICENSE[] SEC("license") = "Dual Apache-2.0/GPL";
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
